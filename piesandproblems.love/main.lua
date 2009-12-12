a125606d4aaee24e1ad0e3d1d32d40a1c3a203d8
Christian Nelson <zero@zerolives.org>
2009-12-12 17:55:07 -0500

    arrows now damage the orc based on x,y coords. plays a little hit sound, takes off orc HP

--- piesandproblems.love/main.lua
+++ piesandproblems.love/main.lua
@@ -1,3 +1,5 @@
+debug = true
+
 screenWidth = 21
 screenHeight = 16
 
@@ -17,7 +19,6 @@ love.graphics.setLineWidth( 4 )
 
 levels = {}
 levels[1] = {name="Kiss The Sky"}
-
 
 -- First two rows are covered by the display
 map = {
@@ -63,11 +64,10 @@ revealedTiles = {
 }
 
 unseenTile = love.graphics.newColor( 24, 24, 24, 255 )
-seenTile = love.graphics.newColor( 24, 24, 24, 0 )
+seenTile = love.graphics.newColor( 24, 24, 24, 100 )
+currentlySeenTile = love.graphics.newColor( 24, 24, 24, 0 )
 
-player = {x=0,y=0,spriteX=0,spriteY=(gridSize*29),vitality='alive',hp=0,str=0,def=1,level=1,status=0,facing='right',angle=0,arrowHave=2}
 baddies = {}
-arrows = {}
 
 downStairsLocationX = math.random(2,screenWidth-1)
 downStairsLocationY = math.random(4,screenHeight-1)
@@ -84,7 +84,6 @@ love.audio.setVolume(volume)
 
 topMenuHeight = gridSize*2
 
-elapsed = 0
 lastkey = 0
 animationsRun = 0
 
@@ -115,19 +114,22 @@ function draw()
 	
 	-- draw UI text
 	love.graphics.setColor( 255, 255, 255 )
-	love.graphics.draw("Orc: "..baddies[1]['x']..", "..baddies[1]['y'], 5, 32)
+	love.graphics.draw("Orc: "..baddies[1]['x']..", "..baddies[1]['y'], 5, 24)
 	love.graphics.draw("HP "..baddies[1]['hp'].."/"..baddies[1]['totalHP'],5,48)
-	love.graphics.draw("Elapsed: "..elapsed, 5, 70)
+	love.graphics.setColor(255,0,0)
+	love.graphics.draw("ATK: "..baddies[1]['atk'],115,48)
+	love.graphics.setColor(0,0,255)
+	love.graphics.draw("DEF: "..baddies[1]['def'],205,48)
+	love.graphics.setColor( 255, 255, 255 )
 	
-	love.graphics.draw("You: "..player['x']..", "..player['y'].." = "..map[player['y']][player['x']], 600, 32)
+	love.graphics.draw("You: "..player['x']..", "..player['y'].." = "..map[player['y']][player['x']], 600, 24)
 	love.graphics.draw("HP: "..player['hp'].."/"..player['totalHP'],600,48)
-	love.graphics.draw("Arrow Have: "..player['arrowHave'],600,70)
-	
-	for i=1,#baddies do
-		if #baddies > 0 then
-			love.graphics.draws( sprites, baddies[i]['x']*gridSize-24, baddies[i]['y']*gridSize-24, baddies[i]['spriteX'], baddies[i]['spriteY'], gridSize*baddies[i]['sizeW'], gridSize*baddies[i]['sizeH'],baddies[i]['angle'])
-		end
-	end
+	love.graphics.setColor(255,0,0)
+	love.graphics.draw("ATK: "..player['atk'],715,48)
+	love.graphics.setColor(0,0,255)
+	love.graphics.draw("DEF: "..player['def'],800,48)
+	love.graphics.setColor( 255, 255, 255 )
+	love.graphics.draw("Arrow Have: "..player['arrowHave'],600,72)
 	
 	for i=1, #arrows do
 		if arrows[i]['isOut'] == true then
@@ -144,12 +146,19 @@ function draw()
 				love.graphics.draws(objects, arrows[i]['x']*gridSize-24,arrows[i]['y']*gridSize-24,385, 240,gridSize,gridSize)
 			end
 		end
-		love.graphics.draw(arrows[i]['x']..", "..arrows[i]['y'],arrows[i]['x']*gridSize-24,arrows[i]['y']*gridSize-24)
+		love.graphics.draw(math.ceil(arrows[i]['x'])..", "..math.ceil(arrows[i]['y']),arrows[i]['x']*gridSize-24,arrows[i]['y']*gridSize-24)
+	end
+	
+	for i=1,#baddies do
+		if #baddies > 0 then
+			love.graphics.draws( sprites, baddies[i]['x']*gridSize-24, baddies[i]['y']*gridSize-24, baddies[i]['spriteX'], baddies[i]['spriteY'], gridSize*baddies[i]['sizeW'], gridSize*baddies[i]['sizeH'],baddies[i]['angle'])
+		end
 	end
 	
 	love.graphics.draws( sprites, player['x']*gridSize-24, player['y']*gridSize-24, player['spriteX'], player['spriteY'], gridSize, gridSize,player['angle'])
 	
-	revealTiles(currentFloor)
+--	revealTiles(currentFloor)
+	bakeLights(currentFloor)
 
 	if optionsOpen == true then
 		love.graphics.setColor( 255, 255, 255 )
@@ -163,34 +172,52 @@ function draw()
 		love.graphics.draw("Volume: " .. (volume*100) .."%", ((width/2)-((width/2)/2))+16, ((height/2)-((height/2)/2))+96)
 	end
 	
+end
+
+function moveBaddie(baddie)
+	
+end
+
+function bakeLights(floor)
 	
 end
 
 function revealTiles(floor)
-	-- draw main background
-	revealedTiles[player['y']][player['x']] = 1
-	revealedTiles[player['y']-1][player['x']] = 1
-	revealedTiles[player['y']+1][player['x']] = 1
-	revealedTiles[player['y']][player['x']-1] = 1
-	revealedTiles[player['y']][player['x']+1] = 1
-	revealedTiles[player['y']+1][player['x']+1] = 1
-	revealedTiles[player['y']-1][player['x']-1] = 1
-	revealedTiles[player['y']+1][player['x']-1] = 1
-	revealedTiles[player['y']+1][player['x']+1] = 1
-	revealedTiles[player['y']-1][player['x']+1] = 1
+	-- Show tiles directly around the player
+
+	for i=1, player['sight'] do
+		revealedTiles[player['y']][player['x']] = 1
+		revealedTiles[player['y']-i][player['x']] = 1
+		revealedTiles[player['y']+i][player['x']] = 1
+		revealedTiles[player['y']][player['x']-i] = 1
+		revealedTiles[player['y']][player['x']+i] = 1
+		revealedTiles[player['y']+i][player['x']+i] = 1
+		revealedTiles[player['y']-i][player['x']-i] = 1
+		revealedTiles[player['y']+i][player['x']-i] = 1
+		revealedTiles[player['y']+i][player['x']+i] = 1
+		revealedTiles[player['y']-i][player['x']+i] = 1
+		revealedTiles[player['y']-i][player['x']-(i/2)] = 1
+		revealedTiles[player['y']+i][player['x']-(i/2)] = 1
+	end
 	
 	for y=3, screenHeight do
 		for x=1, screenWidth do
 			if revealedTiles[y][x] == 0 then
 				love.graphics.setColor(unseenTile)
 				love.graphics.rectangle( 0, (x*gridSize)-gridSize, (y*gridSize)-gridSize,gridSize, gridSize )
-			else
+			end
+			if revealedTiles[y][x] == 1 then
+				love.graphics.setColor(currentlySeenTile)
+				love.graphics.rectangle( 0, (x*gridSize)-gridSize, (y*gridSize)-gridSize,gridSize, gridSize )
+			end
+			if revealedTiles[y][x] == 2 then
 				love.graphics.setColor(seenTile)
 				love.graphics.rectangle( 0, (x*gridSize)-gridSize, (y*gridSize)-gridSize,gridSize, gridSize )
 			end
 		end
 	end
 	love.graphics.setColor(255,255, 255)
+
 end
 
 function moveTowardPlayer(thisX, thisY)
@@ -198,7 +225,6 @@ function moveTowardPlayer(thisX, thisY)
 end
 
 function update(dt)
-	elapsed = math.floor(love.timer.getTime( ))
 	checkArrows(dt)
 end
 
@@ -216,62 +242,91 @@ function checkArrows(dt)
 							if arrows[i]['x'] == baddies[j]['x'] then
 								arrows[i]['x'] = baddies[j]['x']
 								arrows[i]['y'] = baddies[j]['y']
-								arrows[i]['isLive'] = false
-								collision = true
+								if damageBaddie(j) == true then
+									arrows[i]['isLive'] = false
+									collision = true
+								end
 							end
 						end
 					end
 				end
-				
-				if collision == false then
+
+				if arrows[i]['isLive'] == true then
 					arrows[i]['y'] = arrows[i]['y']-.1
 				end
-				
 			end
+			
 			if arrows[i]['firedFrom'] == 'right' then
 				if tileProperties[map[math.ceil(arrows[i]['y'])][math.ceil(arrows[i]['x'])+1]] == 'solid' then
 					arrows[i]['isLive'] = false
 					arrows[i]['x'] = math.ceil(arrows[i]['x'])
 				else
+					for j=1, #baddies do
+						if math.ceil(arrows[i]['x'])+1 == baddies[j]['x'] then
+							if arrows[i]['y'] == baddies[j]['y'] then
+								arrows[i]['x'] = baddies[j]['x']
+								arrows[i]['y'] = baddies[j]['y']
+								if damageBaddie(j) == true then
+									arrows[i]['isLive'] = false
+									collision = true
+								end
+							end
+						end
+					end
+				end
+
+				if arrows[i]['isLive'] == true then
 					arrows[i]['x'] = arrows[i]['x']+.1
 				end
 			end
+			
 			if arrows[i]['firedFrom'] == 'down' then
 				if tileProperties[map[math.ceil(arrows[i]['y'])+1][math.ceil(arrows[i]['x'])]] == 'solid' then
 					arrows[i]['isLive'] = false
 					arrows[i]['y'] = math.ceil(arrows[i]['y'])
 				else
+					for j=1, #baddies do
+						if math.ceil(arrows[i]['y']+1) == baddies[j]['y'] then
+							if arrows[i]['x'] == baddies[j]['x'] then
+								arrows[i]['x'] = baddies[j]['x']
+								arrows[i]['y'] = baddies[j]['y']
+								if damageBaddie(j) == true then
+									arrows[i]['isLive'] = false
+									collision = true
+								end
+							end
+						end
+					end
+				end
+
+				if arrows[i]['isLive'] == true then
 					arrows[i]['y'] = arrows[i]['y']+.1
 				end
 			end
+			
 			if arrows[i]['firedFrom'] == 'left' then
 				if tileProperties[map[math.ceil(arrows[i]['y'])][math.ceil(arrows[i]['x'])-1]] == 'solid' then
 					arrows[i]['isLive'] = false
 					arrows[i]['x'] = math.ceil(arrows[i]['x'])
 				else
+					for j=1, #baddies do
+						if math.ceil(arrows[i]['x']-1) == baddies[j]['x'] then
+							if arrows[i]['y'] == baddies[j]['y'] then
+								arrows[i]['x'] = baddies[j]['x']
+								arrows[i]['y'] = baddies[j]['y']
+								if damageBaddie(j) == true then
+									arrows[i]['isLive'] = false
+									collision = true
+								end
+							end
+						end
+					end
+				end
+
+				if arrows[i]['isLive'] == true then
 					arrows[i]['x'] = arrows[i]['x']-.1
 				end
 			end
-		end
-		
-	end
-end
-
-function baddieHitByArrow(arrow,baddie)
-	if math.random(1,100) < 75 then
-		arrows[arrow]['isLive'] = false
-		if arrows[arrow]['firedFrom'] == 'left' then
-			arrows[arrow]['y'] = baddies[baddie]['y']
-			arrows[arrow]['x'] = arrows[arrow]['x']
-		end
-		if arrows[arrow]['firedFrom'] == 'right' then
-			arrows[arrow]['y'] = baddies[baddie]['y']
-		end
-		if arrows[arrow]['firedFrom'] == 'up' then
-			arrows[arrow]['x'] = baddies[baddie]['x']
-		end
-		if arrows[arrow]['firedFrom'] == 'down' then
-			arrows[arrow]['x'] = baddies[baddie]['x']
 		end
 	end
 end
@@ -343,15 +398,50 @@ function loadSounds()
 	mysteriousItem = love.audio.newSound('MysteriousItem1.wav')
 end
 
-function damageBaddie(damage,baddie)
-	damageTaken = damage+player['str']
+function damageBaddie(baddie)
+	attackRoll = math.random(0,player['atk'])
+	defRoll = math.random(0,baddies[baddie]['def'])
+	if attackRoll > defRoll then
+		damageTaken = attackRoll - defRoll
+		baddies[baddie]['hp'] = baddies[baddie]['hp']-damageTaken
+		love.audio.play(weaponHitSound2)
+		return true
+	else
+		playerMissed = true
+		return false
+	end
+end
+
+function diceRoll(n)
+	total = 0
+	for i=1, n do
+		total = total+math.random(1,6)
+	end
+	
+	return total
+end
+
+function levelUp(char)
+	if char == 'player' then
+		player['level'] = player['level']+1
+		bump = math.random(player['level'],(player['level']*2))
+		player['totalHP'] = player['totalHP'] + bump
+		player['hp'] = player['totalHP']
+	else
+		baddies[char]['level'] = baddies[char]['level']+1
+		bump = math.random(baddies[char]['level'],(baddies[char]['level']*2))
+		baddies[char]['totalHP'] = baddies[char]['totalHP'] + bump
+		baddies[char]['hp'] = baddies[char]['totalHP']
+	end
 end
 
 function spawnPlayer()
-	player['hp'] = math.random(5,10)
+	player = {x=0,y=0,spriteX=0,spriteY=(gridSize*29),vitality='alive',hp=0,atk=diceRoll(1),def=diceRoll(1),level=1,status=0,facing='right',angle=0,arrowHave=2,sight=2}
+	arrows = {}
+	playerMissed = false
+
+	player['hp'] = diceRoll(2)
 	player['totalHP'] = player['hp']
-	player['str'] = math.random(1,2)
-	player['def'] = math.random(1,2)
 	
 	-- Position character
 	player['x'] = getGoodX(1)
@@ -359,13 +449,13 @@ function spawnPlayer()
 
 	player['facing'] = "right"
 	
-	player['arrowHave'] = math.random(1,5)
+	player['arrowHave'] = 5
 end
 
 function spawnBaddie(type)
 	i = #baddies+1
 	if type == 'orc' then
-		baddies[i] = {x=0,y=0,spriteX=(gridSize*10),spriteY=(gridSize*8),vitality='alive',hp=math.random(5,10),str=math.random(1,2),def=math.random(2,4),level=1,status=0,facing='right',angle=0,sizeH=1,sizeW=1}
+		baddies[i] = {x=0,y=0,spriteX=(gridSize*10),spriteY=(gridSize*8),vitality='alive',hp=diceRoll(2),atk=diceRoll(1),def=diceRoll(1),level=1,status=0,facing='right',angle=0,sizeH=1,sizeW=1}
 		
 		baddies[i]['totalHP'] = baddies[i]['hp']
 	end
@@ -381,9 +471,6 @@ end
 
 function charAttack()
 	love.audio.play(playerSwordSound)
-	if math.random(0,100) <= swordStats['tohit'] then
-		damageBaddie(swordStats['dmg']*(player['str']/2))
-	end
 	turn = turn+1
 end
 
