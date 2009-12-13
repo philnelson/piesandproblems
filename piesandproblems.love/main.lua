86b773416d1ca8a4d0cacbc8cc11ef45332d742f
Christian Nelson <zero@zerolives.org>
2009-12-13 12:26:12 -0500

    initial room generation try

--- piesandproblems.love/main.lua
+++ piesandproblems.love/main.lua
@@ -49,9 +49,6 @@ currentlySeenTile = love.graphics.newColor( 24, 24, 24, 0 )
 
 baddies = {}
 
-downStairsLocationX = math.random(2,screenWidth-1)
-downStairsLocationY = math.random(4,screenHeight-1)
-
 turn = 0
 currentFloor = 0
 
@@ -66,6 +63,11 @@ animationsRun = 0
 
 optionsOpen = false
 
+baddiesAlive = 0
+
+doorOpened = false
+mapGenerated = false
+
 function load()
 	spawnPlayer()
 	
@@ -77,13 +79,19 @@ function load()
 	font = love.graphics.newFont('04B_03__.TTF', 24)
 	love.graphics.setFont(font)
 	
-	sprites = love.graphics.newImage( 'lofi_char_a_48.png' )
-	environment = love.graphics.newImage( 'lofi_environment_a_48.png' )
-	objects = love.graphics.newImage( 'lofi_obj_a_48.png' )
-	portraits = love.graphics.newImage( 'lofi_portrait_a_48.png' )
-	interface = love.graphics.newImage( 'lofi_interface_a_48.png' )
+	loadGraphics()
 
 	--love.audio.play( overWorldTheme, 0 )
+end
+
+function loadGraphics()
+	spriteRowOffsets = {sprites=0,environment=31,interface=47,objects=63,portraits=79}
+
+	sprites = love.graphics.newImage( 'lofi_char_a_48.png' )
+	environment = love.graphics.newImage( 'lofi_environment_a_48.png' )
+	interface = love.graphics.newImage( 'lofi_interface_a_48.png' )
+	objects = love.graphics.newImage( 'lofi_obj_a_48.png' )
+	portraits = love.graphics.newImage( 'lofi_portrait_a_48.png' )
 end
 
 function draw()
@@ -164,14 +172,17 @@ function draw()
 		for i=1, #messages do
 			if messages[i]['alpha'] > 0 then
 				love.graphics.setColor( 0, 0, 0, messages[i]['alpha'] )
-				love.graphics.draw(messages[i]['message'],(messages[i]['x']*gridSize)-(gridSize*2),(messages[i]['y']*gridSize))
 				if messages[i]['type'] == 'get' then
+					love.graphics.draw(messages[i]['message'],(messages[i]['x']*gridSize)-(gridSize*2)+2,(messages[i]['y']*gridSize)+2)
 					love.graphics.setColor( 255, 255, 255, messages[i]['alpha'] )
+					love.graphics.draw(messages[i]['message'],(messages[i]['x']*gridSize)-(gridSize*2),(messages[i]['y']*gridSize))
 				end
 				if messages[i]['type'] == 'damage' then
+					love.graphics.draw(messages[i]['message'],(messages[i]['x']*gridSize)-(gridSize)+2,(messages[i]['y']*gridSize)+2)
 					love.graphics.setColor( 255, 0, 0, messages[i]['alpha'] )
+					love.graphics.draw(messages[i]['message'],(messages[i]['x']*gridSize)-(gridSize),(messages[i]['y']*gridSize))
 				end
-				love.graphics.draw(messages[i]['message'],(messages[i]['x']*gridSize)-(gridSize*2)+3,(messages[i]['y']*gridSize)+3)
+				
 			end
 		end
 	end
@@ -214,6 +225,23 @@ function update(dt)
 			messages[i]['y'] = messages[i]['y']-.01
 		end
 	end
+	
+	for i=1, #baddies do
+		moveBaddie(i)
+	end
+	
+	if doorOpened == false then
+		if baddiesAlive == 0 then
+			doorOpen = love.audio.newSound('shaktool_yowzer_event_1.wav')
+			openDoor()
+			love.audio.play(doorOpen)
+		end
+	end
+	
+end
+
+function openDoor()
+	doorOpened = true
 end
 
 function bakeLights(floor)
@@ -447,6 +475,13 @@ function keypressed(key)
 	if key == love.key_q then
 		love.system.exit()
 	end
+	if key == love.key_d then
+		if debug == true then
+			debug = false
+		else
+			debug = true
+		end
+	end
 	if key == 57 then
 		if volume > .1 then
 			volume = volume - .1
@@ -485,6 +520,7 @@ function loadSounds()
 	ouch1 = love.audio.newSound('shaktool_yowzer_ouch_2.wav')
 	arrowMissSound = love.audio.newSound('shaktool_yowzer_thud_2.wav')
 	deathRattle = love.audio.newSound('shaktool_yowzer_blubber_1.wav')
+	arrowPickup = love.audio.newSound('SFRX/spawn1.wav')
 
 	uiOpenSound = love.audio.newSound('Stian_Stark_SFX_Pack_Vol_1/open01.wav')
 	
@@ -492,8 +528,8 @@ function loadSounds()
 end
 
 function damageBaddie(baddie,kind)
-	attackRoll = math.random(0,player['atk'])
-	defRoll = math.random(0,baddies[baddie]['def'])
+	attackRoll = math.random(1,player['atk'])+1
+	defRoll = math.random(1,baddies[baddie]['def'])
 	if attackRoll > defRoll then
 		damageTaken = attackRoll - defRoll
 		baddies[baddie]['hp'] = baddies[baddie]['hp']-damageTaken
@@ -505,15 +541,19 @@ function damageBaddie(baddie,kind)
 			love.audio.play(weaponHitSound)
 		end
 		if baddies[baddie]['hp'] <= 0 then
-			baddies[baddie]['hp'] = 0
-			baddies[baddie]['vitality'] = 'dead'
-			baddieDied = love.timer.getTime()
+			killBaddie(baddie)
 		end
 		return true
 	else
 		playerMissed = true
 		return false
 	end
+end
+
+function killBaddie(baddie)
+	baddies[baddie]['hp'] = 0
+	baddies[baddie]['vitality'] = 'dead'
+	baddiesAlive = baddiesAlive-1
 end
 
 function diceRoll(n,minimum)
@@ -565,6 +605,7 @@ function spawnBaddie(type)
 		baddies[i] = {x=0,y=0,spriteX=(gridSize*10),spriteY=(gridSize*8),vitality='alive',hp=diceRoll(2,3),atk=diceRoll(1,3),def=diceRoll(1,3),level=1,status=0,facing='right',angle=0,sizeH=1,sizeW=1,sawPlayer=false,scale=1,deathAnimation=0,deathSpriteX=(15*gridSize),deathSpriteY=(9*gridSize)}
 		
 		baddies[i]['totalHP'] = baddies[i]['hp']
+		baddiesAlive = baddiesAlive+1
 	end
 
 	goodPos = false
@@ -576,11 +617,6 @@ function spawnBaddie(type)
 	end
 end
 
-function charAttack()
-	love.audio.play(playerSwordSound)
-	turn = turn+1
-end
-
 function playerShoot()
 	if player['arrowHave'] ~= 0 then
 		if player['canShoot'] == true then
@@ -588,6 +624,8 @@ function playerShoot()
 			player['arrowHave'] = player['arrowHave']-1
 			turn = turn+1
 		end
+	else
+		messages[#messages+1] = {x=player['x'],y=player['y'],message="No Arrow Have!",alpha=255,started=0,type='get'}
 	end
 end
 
@@ -607,8 +645,12 @@ function movePlayer(direction)
 				if baddies[i]['vitality'] == 'alive' then
 					if player['x']-1 == baddies[i]['x'] then
 						if player['y'] == baddies[i]['y'] then
-							collision = true
-							damageBaddie(i,'sword')
+							if damageBaddie(i,'sword') == true then
+								collision = true
+							else
+								collision = true
+								messages[#messages+1] = {x=baddies[i]['x'],y=baddies[i]['y'],message="Miss!",alpha=255,started=0,type='get'}
+							end
 						end
 					end
 				end
@@ -629,8 +671,12 @@ function movePlayer(direction)
 				if baddies[i]['vitality'] == 'alive' then
 					if player['x']+1 == baddies[i]['x'] then
 						if player['y'] == baddies[i]['y'] then
-							collision = true
-							damageBaddie(i,'sword')
+							if damageBaddie(i,'sword') == true then
+								collision = true
+							else
+								collision = true
+								messages[#messages+1] = {x=baddies[i]['x'],y=baddies[i]['y'],message="Miss!",alpha=255,started=0,type='get'}
+							end
 						end
 					end
 				end
@@ -651,8 +697,12 @@ function movePlayer(direction)
 				if baddies[i]['vitality'] == 'alive' then
 					if player['x'] == baddies[i]['x'] then
 						if player['y']-1 == baddies[i]['y'] then
-							collision = true
-							damageBaddie(i,'sword')
+							if damageBaddie(i,'sword') == true then
+								collision = true
+							else
+								collision = true
+								messages[#messages+1] = {x=baddies[i]['x'],y=baddies[i]['y'],message="Miss!",alpha=255,started=0,type='get'}
+							end
 						end
 					end
 				end
@@ -673,8 +723,12 @@ function movePlayer(direction)
 				if baddies[i]['vitality'] == 'alive' then
 					if player['x'] == baddies[i]['x'] then
 						if player['y']+1 == baddies[i]['y'] then
-							collision = true
-							damageBaddie(i,'sword')
+							if damageBaddie(i,'sword') == true then
+								collision = true
+							else
+								collision = true
+								messages[#messages+1] = {x=baddies[i]['x'],y=baddies[i]['y'],message="Miss!",alpha=255,started=0,type='get'}
+							end
 						end
 					end
 				end
@@ -691,21 +745,29 @@ function movePlayer(direction)
 end
 
 function checkForPickups()
+	arrowsGot = 0
 	for i=1, #arrows do
 		if arrows[i]['x'] == player['x'] then
 			if arrows[i]['y'] == player['y'] then
 				if arrows[i]['isOut'] == true then
 					arrowGet(i)
+					arrowsGot = arrowsGot+1
 				end
 			end
 		end
+	end
+	if arrowsGot == 1 then
+		messages[#messages+1] = {x=player['x'],y=player['y'],message="Arrow Get!!",alpha=255,started=0,type='get'}
+	end
+	if arrowsGot > 1 then
+		messages[#messages+1] = {x=player['x'],y=player['y'],message="Arrow Get!! x"..arrowsGot,alpha=255,started=0,type='get'}
 	end
 end
 
 function arrowGet(id)
 	arrows[id]['isOut'] = false
 	player['arrowHave'] = player['arrowHave']+1
-	messages[#messages+1] = {x=player['x'],y=player['y'],message="Arrow Get!",alpha=255,started=0,type='get'}
+	love.audio.play(arrowPickup)
 end
 
 function moveBaddie(baddie)
@@ -723,11 +785,48 @@ end
 
 function generateRoom(floor)
 	require('levels/'..floor..'.lua')
-	doorOpened = false
+	
+	for y=3, screenHeight do
+		for x=1, screenWidth do
+			if map[y][x] == 9 then
+				map[y][x] = tileTypes[9][1]
+				downStairsX = x
+				downStairsY = y
+			end
+			if map[y][x] == 8 then
+				
+			end
+			if map[y][x] == 7 then
+			
+			end
+			if map[y][x] == 6 then
+			
+			end
+			if map[y][x] == 5 then
+			
+			end
+			if map[y][x] == 4 then
+			
+			end
+			if map[y][x] == 3 then
+				map[y][x] = tileTypes[3][math.random(1,#tileTypes[3])]
+			end
+			if map[y][x] == 2 then
+				map[y][x] = 496
+			end
+			if map[y][x] == 1 then
+			
+			end
+			if map[y][x] == 0 then
+				map[y][x] = tileTypes[0][math.random(1,#tileTypes[0])]
+			end
+		end
+	end
+	
 	if doorOpened == true then
-		map[downStairsLocationY][downStairsLocationX] = exitOpenTile
+		map[downStairsY][downStairsX] = tileTypes[9][2]
 	else
-		map[downStairsLocationY][downStairsLocationX] = exitClosedTile
+		map[downStairsY][downStairsX] = tileTypes[9][1]
 	end
 	
 	-- draw main background
@@ -739,8 +838,8 @@ function generateRoom(floor)
 
 	for y=3, screenHeight do
 		for x=1, screenWidth do
-			envSpriteY = math.floor((map[y][x]*48)/768)
-			envSpriteX = map[y][x]
+			envSpriteY = math.ceil(((map[y][x]-(spriteRowOffsets['environment']*16))*48)/768)
+			envSpriteX = math.floor((map[y][x]-(spriteRowOffsets['environment']*16)))-16
 			while (envSpriteX > 16) do
 				envSpriteX = envSpriteX - 16
 			end
@@ -786,4 +885,9 @@ function checkSpaceEmpty(x,y,id)
 	else
 		return true
 	end
+end
+
+function round(num, idp)
+  local mult = 10^0
+  return math.floor(num * mult + 0.5) / mult
 end
