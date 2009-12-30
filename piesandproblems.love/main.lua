5304efe2d8349c20038e744d58a9621e666423cb
Christian Nelson <zero@zerolives.org>
2009-12-30 12:36:25 -0500

    No idea

--- piesandproblems.love/main.lua
+++ piesandproblems.love/main.lua
@@ -22,7 +22,6 @@ love.graphics.setLineStyle( love.line_rough )
 love.graphics.setLineWidth( 4 )
 
 levels = {}
-levels[1] = {name="Kiss The Sky"}
 messages = {}
 
 revealedTiles = {
@@ -76,7 +75,9 @@ moves.path = {{2,11},{3,10},{5,10},{6,11},{6,13},{5,14},{3,14},{2,13}}
 moves.xsize=table.getn(moves.path[1]) -- horizontal map size
 moves.ysize=table.getn(moves.path)	   -- vertical map size
 
-testo =  "status"
+lootTypes = {'armor','weapon','gold','trap'}
+
+levelUpThresholds = {0,10,20,40,80,160}
 
 function load()
 	spawnPlayer()
@@ -102,12 +103,8 @@ function loadGraphics()
 end
 
 function draw()
-	generateRoom(currentFloor)
-	
-	if #baddies < numberOfBaddies then
-		spawnBaddie('orc')
-	end
-	
+	initLevel()
+
 	-- draw UI text
 	love.graphics.setColor( 255, 255, 255 )
 	love.graphics.draw("Orc: "..baddies[1]['x']..", "..baddies[1]['y'].." ("..getSpaceFromXY(baddies[1]['x'],baddies[1]['y'])..")", 5, 24)
@@ -123,7 +120,7 @@ function draw()
 	playerSpace = getSpaceFromXY(player['x'],player['y'])
 	playerXY = getXYFromSpace(playerSpace)
 	
-	love.graphics.draw("You: "..player['x']..", "..player['y'].." ("..playerSpace..") "..tileProperties[map[player['y']][player['x']]], 600, 24)
+	love.graphics.draw("You: "..player['x']..", "..player['y'].." ("..playerSpace..") "..tileProperties[map.array[player['y']][player['x']]], 600, 24)
 	love.graphics.draw("HP: "..player['hp'].."/"..player['totalHP'],600,48)
 	love.graphics.setColor(255,0,0)
 	love.graphics.draw("ATK: "..player['atk'],715,48)
@@ -149,7 +146,10 @@ function draw()
 		end
 	end
 	
-	love.graphics.draws( sprites, player['x']*gridSize-24, player['y']*gridSize-24, player['spriteX'], player['spriteY'], gridSize, gridSize,player['angle'])
+	if player['vitality'] == 'dead' then
+		player['angle'] = -90
+	end
+	love.graphics.draws( sprites, player['x']*gridSize-24, player['y']*gridSize-24, player['spriteX'], player['spriteY'], gridSize, gridSize,player['angle'],player['scale'])
 	
 --	revealTiles(currentFloor)
 	bakeLights(currentFloor)
@@ -198,25 +198,12 @@ function draw()
 
 	for k in pairs(baddies) do
 		if baddies[k]['vitality'] == 'alive' then
-			if baddies[k]['lastMove'] <= (love.timer.getTime()-.7) then
+			if baddies[k]['lastMove'] <= (love.timer.getTime()-.8) then
 				baddies[k]['lastMove'] = love.timer.getTime()
 				moveBaddie(k)
 			end
 		end
 	end	
-
-	testo =  "Path: "
-	if moves.path == nil then
-		testo = "NO PATH"
-	else
-		local color1 = love.graphics.getColor()
-		--love.graphics.setColor( 100, 205, 0 ) 
-		for i,v in ipairs(moves.path ) do
-			testo = testo .. " ( "  .. tostring(v.x) .. " , " .. tostring(v.y) .. " ) "
-		end
-		love.graphics.setColor( color1) 
-	end
-	love.graphics.draw(testo  ,10,110)
 
 end
 
@@ -241,6 +228,24 @@ function update(dt)
 			end
 		end
 	end
+	
+	if player['vitality'] == 'dead' then
+		if player['deathAnimation'] == 2 then
+			player['scale'] = player['scale']-.1
+			if player['scale'] <= 1 then
+				player['scale'] = 1
+				player['deathAnimation'] = 3
+				love.audio.play(deathRattle)
+			end
+		end
+		if player['deathAnimation'] == 0 then
+			player['scale'] = player['scale']+.1
+			if player['scale'] >= 2 then
+				player['deathAnimation'] = 2
+			end
+		end
+	end
+	
 	if love.joystick.getNumJoysticks( ) > 0 then
 		checkJoystick()
 	end
@@ -265,6 +270,14 @@ function update(dt)
 		end
 	end
 	
+end
+
+function initLevel()
+	generateRoom(currentFloor)
+	
+	if #baddies < numberOfBaddies then
+		spawnBaddie('orc')
+	end
 end
 
 function openDoor()
@@ -376,7 +389,7 @@ function checkArrows(dt)
 						end
 					end
 				end
-				if tileProperties[map[math.ceil(arrows[i]['y'])-1][math.ceil(arrows[i]['x'])]] == 'solid' then
+				if tileProperties[map.array[math.ceil(arrows[i]['y'])-1][math.ceil(arrows[i]['x'])]] == 'solid' then
 					arrows[i]['isLive'] = false
 					arrows[i]['y'] = math.ceil(arrows[i]['y'])
 				end
@@ -403,7 +416,8 @@ function checkArrows(dt)
 						end
 					end
 				end
-				if tileProperties[map[math.ceil(arrows[i]['y'])][math.ceil(arrows[i]['x'])+1]] == 'solid' then
+				
+				if tileProperties[map.array[math.ceil(arrows[i]['y'])][math.ceil(arrows[i]['x'])+1]] == 'solid' then
 					arrows[i]['isLive'] = false
 					arrows[i]['x'] = math.ceil(arrows[i]['x'])
 				end
@@ -429,7 +443,7 @@ function checkArrows(dt)
 						end
 					end
 				end
-				if tileProperties[map[math.ceil(arrows[i]['y'])+1][math.ceil(arrows[i]['x'])]] == 'solid' then
+				if tileProperties[map.array[math.ceil(arrows[i]['y'])+1][math.ceil(arrows[i]['x'])]] == 'solid' then
 					arrows[i]['isLive'] = false
 					arrows[i]['y'] = math.ceil(arrows[i]['y'])	
 				end
@@ -456,7 +470,7 @@ function checkArrows(dt)
 						end
 					end
 				end
-				if tileProperties[map[math.ceil(arrows[i]['y'])][math.ceil(arrows[i]['x'])-1]] == 'solid' then
+				if tileProperties[map.array[math.ceil(arrows[i]['y'])][math.ceil(arrows[i]['x'])-1]] == 'solid' then
 					arrows[i]['isLive'] = false
 					arrows[i]['x'] = math.ceil(arrows[i]['x'])				
 				end
@@ -517,6 +531,11 @@ function killBaddie(baddie)
 	baddiesKilled = baddiesKilled+1
 end
 
+function killPlayer()
+	player['hp'] = 0
+	player['vitality'] = 'dead'
+end
+
 function diceRoll(n,minimum)
 	total = 0
 	for i=1, n do
@@ -544,11 +563,11 @@ function levelUp(char)
 end
 
 function spawnPlayer()
-	player = {x=0,y=0,spriteX=0,spriteY=(gridSize*29),vitality='alive',hp=0,atk=diceRoll(1,3),def=diceRoll(1,3),level=1,status=0,facing='right',angle=0,arrowHave=2,sight=2,canShoot=true,liveArrows=0}
+	player = {x=0,y=0,spriteX=0,spriteY=(gridSize*29),vitality='alive',hp=0,atk=diceRoll(1,3),def=diceRoll(1,3),level=1,status=0,facing='right',angle=0,arrowHave=2,sight=2,canShoot=true,liveArrows=0,deathAnimation=0,scale=1}
 	arrows = {}
 	playerMissed = false
 
-	player['hp'] = diceRoll(2,3)
+	player['hp'] = diceRoll(3,2)
 	player['totalHP'] = player['hp']
 	
 	-- Position character
@@ -563,7 +582,7 @@ end
 function spawnBaddie(type)
 	i = #baddies+1
 	if type == 'orc' then
-		baddies[i] = {x=0,y=0,spriteX=(gridSize*10),spriteY=(gridSize*8),vitality='alive',hp=diceRoll(2,3),atk=diceRoll(1,3),def=diceRoll(1,3),level=1,status=0,facing='right',angle=0,sizeH=1,sizeW=1,lastSawPlayer='none',scale=1,deathAnimation=0,deathSpriteX=(15*gridSize),deathSpriteY=(9*gridSize),canSeePlayer=false,lastMove=0}
+		baddies[i] = {x=0,y=0,spriteX=(gridSize*10),spriteY=(gridSize*8),vitality='alive',hp=diceRoll(3,2),atk=diceRoll(1,3),def=diceRoll(1,3),level=1,status=0,facing='right',angle=0,sizeH=1,sizeW=1,lastSawPlayer='none',scale=1,deathAnimation=0,deathSpriteX=(15*gridSize),deathSpriteY=(9*gridSize),canSeePlayer=false,lastMove=0}
 		
 		baddies[i]['totalHP'] = baddies[i]['hp']
 		baddiesAlive = baddiesAlive+1
@@ -575,6 +594,13 @@ function spawnBaddie(type)
 		baddies[i]['x'] = getGoodX(baddies[i]['sizeW'])
 		baddies[i]['y'] = getGoodY(baddies[i]['sizeH'])
 		goodPos = checkXYEmpty(baddies[i]['x'],baddies[i]['y'],i)
+	end
+end
+
+function spawnLoot(lx,ly)
+	if currentFloor <= 3 then
+		lootType = types[rand(1,#types)]
+		loot[#loot+1] = {x=lx,y=ly,spriteX=0,spriteY=0,spriteH=48,spriteW=48,name=getItemName(lootType)}
 	end
 end
 
@@ -597,112 +623,114 @@ function spawnArrow()
 end
 
 function movePlayer(direction)
-	collision = false
-	if direction == "left" then
-		player['spriteX'] = 96
-		if tileProperties[map[player['y']][(player['x'])-1]] ~= 'solid' then
+	if player['vitality'] == 'alive' then
+		collision = false
+		if direction == "left" then
+			player['spriteX'] = 96
+			if tileProperties[map.array[player['y']][(player['x'])-1]] ~= 'solid' then
 			
-			for i=1, #baddies do
-				if baddies[i]['vitality'] == 'alive' then
-					if player['x']-1 == baddies[i]['x'] then
-						if player['y'] == baddies[i]['y'] then
-							if damageBaddie(i,'sword') == true then
-								collision = true
-							else
-								collision = true
-								messages[#messages+1] = {x=baddies[i]['x'],y=baddies[i]['y'],message="Miss!",alpha=255,started=0,type='get'}
+				for i=1, #baddies do
+					if baddies[i]['vitality'] == 'alive' then
+						if player['x']-1 == baddies[i]['x'] then
+							if player['y'] == baddies[i]['y'] then
+								if damageBaddie(i,'sword') == true then
+									collision = true
+								else
+									collision = true
+									messages[#messages+1] = {x=baddies[i]['x'],y=baddies[i]['y'],message="Miss!",alpha=255,started=0,type='get'}
+								end
 							end
 						end
 					end
 				end
-			end
 			
-			if collision == false then
-				player['x'] = player['x']-1 
+				if collision == false then
+					player['x'] = player['x']-1 
+				end
 			end
+			player['facing'] = "left"
+			turn = turn+1;
 		end
-		player['facing'] = "left"
-		turn = turn+1;
-	end
 
-	if direction == "right" then
-		player['spriteX'] = 0
-		if tileProperties[map[player['y']][(player['x'])+1]] ~= 'solid' then
-			for i=1, #baddies do
-				if baddies[i]['vitality'] == 'alive' then
-					if player['x']+1 == baddies[i]['x'] then
-						if player['y'] == baddies[i]['y'] then
-							if damageBaddie(i,'sword') == true then
-								collision = true
-							else
-								collision = true
-								messages[#messages+1] = {x=baddies[i]['x'],y=baddies[i]['y'],message="Miss!",alpha=255,started=0,type='get'}
+		if direction == "right" then
+			player['spriteX'] = 0
+			if tileProperties[map.array[player['y']][(player['x'])+1]] ~= 'solid' then
+				for i=1, #baddies do
+					if baddies[i]['vitality'] == 'alive' then
+						if player['x']+1 == baddies[i]['x'] then
+							if player['y'] == baddies[i]['y'] then
+								if damageBaddie(i,'sword') == true then
+									collision = true
+								else
+									collision = true
+									messages[#messages+1] = {x=baddies[i]['x'],y=baddies[i]['y'],message="Miss!",alpha=255,started=0,type='get'}
+								end
 							end
 						end
 					end
 				end
-			end
 			
-			if collision == false then
-				player['x'] = player['x']+1 
+				if collision == false then
+					player['x'] = player['x']+1 
+				end
 			end
+			player['facing'] = "right"
+			turn = turn+1;
 		end
-		player['facing'] = "right"
-		turn = turn+1;
-	end
 
-	if direction == "up" then
-		player['spriteX'] = 144
-		if tileProperties[map[player['y']-1][(player['x'])]] ~= 'solid' then
-			for i=1, #baddies do
-				if baddies[i]['vitality'] == 'alive' then
-					if player['x'] == baddies[i]['x'] then
-						if player['y']-1 == baddies[i]['y'] then
-							if damageBaddie(i,'sword') == true then
-								collision = true
-							else
-								collision = true
-								messages[#messages+1] = {x=baddies[i]['x'],y=baddies[i]['y'],message="Miss!",alpha=255,started=0,type='get'}
+		if direction == "up" then
+			player['spriteX'] = 144
+			if tileProperties[map.array[player['y']-1][(player['x'])]] ~= 'solid' then
+				for i=1, #baddies do
+					if baddies[i]['vitality'] == 'alive' then
+						if player['x'] == baddies[i]['x'] then
+							if player['y']-1 == baddies[i]['y'] then
+								if damageBaddie(i,'sword') == true then
+									collision = true
+								else
+									collision = true
+									messages[#messages+1] = {x=baddies[i]['x'],y=baddies[i]['y'],message="Miss!",alpha=255,started=0,type='get'}
+								end
 							end
 						end
 					end
 				end
-			end
 			
-			if collision == false then
-				player['y'] = player['y']-1 
+				if collision == false then
+					player['y'] = player['y']-1 
+				end
 			end
+			player['facing'] = "up"
+			turn = turn+1;
 		end
-		player['facing'] = "up"
-		turn = turn+1;
-	end
 
-	if direction == "down" then
-		player['spriteX'] = 48
-		if tileProperties[map[player['y']+1][(player['x'])]] ~= 'solid' then
-			for i=1, #baddies do
-				if baddies[i]['vitality'] == 'alive' then
-					if player['x'] == baddies[i]['x'] then
-						if player['y']+1 == baddies[i]['y'] then
-							if damageBaddie(i,'sword') == true then
-								collision = true
-							else
-								collision = true
-								messages[#messages+1] = {x=baddies[i]['x'],y=baddies[i]['y'],message="Miss!",alpha=255,started=0,type='get'}
+		if direction == "down" then
+			player['spriteX'] = 48
+			if tileProperties[map.array[player['y']+1][(player['x'])]] ~= 'solid' then
+				for i=1, #baddies do
+					if baddies[i]['vitality'] == 'alive' then
+						if player['x'] == baddies[i]['x'] then
+							if player['y']+1 == baddies[i]['y'] then
+								if damageBaddie(i,'sword') == true then
+									collision = true
+								else
+									collision = true
+									messages[#messages+1] = {x=baddies[i]['x'],y=baddies[i]['y'],message="Miss!",alpha=255,started=0,type='get'}
+								end
 							end
 						end
 					end
 				end
-			end
 			
-			if collision == false then
-				player['y'] = player['y']+1
+				if collision == false then
+					player['y'] = player['y']+1
+				end
 			end
+			player['facing'] = "down"
+			turn = turn+1;
 		end
-		player['facing'] = "down"
-		turn = turn+1;
+		checkForPickups()
 	end
-	checkForPickups()
 end
 
 function animateArrows()
@@ -754,42 +782,34 @@ function arrowGet(id)
 end
 
 function moveBaddie(baddie)
-	moves.path = CalcPath(CalcMoves(mappy.array,baddies[baddie]['x'],baddies[baddie]['y'],player['x'],player['y']))
-	if moves.path[1]['x'] == player['x'] then
-		if moves.path[1]['y'] == player['y'] then
-			attackPlayer(baddie)
+	moves.path = CalcPath(CalcMoves(map.array,baddies[baddie]['x'],baddies[baddie]['y'],player['x'],player['y']))
+		
+	if moves.path ~= nil then
+		if moves.path[1]['x'] == player['x'] and moves.path[1]['y'] == player['y'] then
+				attackPlayer(baddie)
+				return true
 		end
+		baddies[baddie]['x'] = moves.path[1]['x']
+		baddies[baddie]['y'] = moves.path[1]['y']
 	end
-	for k in pairs(baddies) do
-		if moves.path[1]['x'] == baddies[k]['x'] then
-			if moves.path[1]['y'] == baddies[k]['y'] then
-				return nil
-			end
-		end
-	end
-	baddies[baddie]['x'] = moves.path[1]['x']
-	baddies[baddie]['y'] = moves.path[1]['y']
+	moves.path = nil
 end
 
-function attackPlayer()
-	attackRoll = math.random(1,baddie['atk'])+1
+function attackPlayer(baddie)
+	attackRoll = math.random(1,baddies[baddie]['atk'])+1
 	defRoll = math.random(1,player['def'])
 	if attackRoll > defRoll then
 		damageTaken = diceRoll(1,1)
 		player['hp'] = player['hp']-damageTaken
 		messages[#messages+1] = {x=player['x'],y=player['y'],message="-"..damageTaken,alpha=255,started=0,type='damage'}
-		if kind == 'arrow' then
-			love.audio.play(ouch1)
-		end
-		if kind == 'sword' then
-			love.audio.play(weaponHitSound)
-		end
+		love.audio.play(weaponHitSound)
 		if player['hp'] <= 0 then
 			killPlayer()
 		end
 		return true
 	else
 		baddieMissed = true
+		messages[#messages+1] = {x=player['x'],y=player['y'],message="Miss!",alpha=255,started=0,type='get'}
 		return false
 	end
 end
@@ -808,46 +828,48 @@ function generateRoom(floor)
 	
 	for y=3, screenHeight do
 		for x=1, screenWidth do
-			if map[y][x] == 9 then
-				map[y][x] = tileTypes[9][1]
+			if map.array[y][x] == 9 then
+				map.array[y][x] = tileTypes[9][1]
 				downStairsX = x
 				downStairsY = y
 			end
-			if map[y][x] == 8 then
+			if map.array[y][x] == 8 then
 				
 			end
-			if map[y][x] == 7 then
+			if map.array[y][x] == 7 then
 			
 			end
-			if map[y][x] == 6 then
+			if map.array[y][x] == 6 then
 			
 			end
-			if map[y][x] == 5 then
+			if map.array[y][x] == 5 then
 			
 			end
-			if map[y][x] == 4 then
+			if map.array[y][x] == 4 then
 			
 			end
-			if map[y][x] == 3 then
-				--map[y][x] = tileTypes[3][math.random(1,#tileTypes[3])]
-				map[y][x] = 512
+			if map.array[y][x] == 3 then
+				map.array[y][x] = tileTypes[0][math.random(1,#tileTypes[0])]
+				if math.random(1,4) > 1 then
+					map.array[y][x] = 512
+				end
 			end
-			if map[y][x] == 2 then
-				map[y][x] = 496
+			if map.array[y][x] == 2 then
+				map.array[y][x] = 496
 			end
-			if map[y][x] == 1 then
+			if map.array[y][x] == 1 then
 			
 			end
-			if map[y][x] == 0 then
-				map[y][x] = tileTypes[0][math.random(1,#tileTypes[0])]
+			if map.array[y][x] == 0 then
+				map.array[y][x] = tileTypes[0][math.random(1,#tileTypes[0])]
 			end
 		end
 	end
 	
 	if doorOpened == true then
-		map[downStairsY][downStairsX] = tileTypes[9][2]
+		map.array[downStairsY][downStairsX] = tileTypes[9][2]
 	else
-		map[downStairsY][downStairsX] = tileTypes[9][1]
+		map.array[downStairsY][downStairsX] = tileTypes[9][1]
 	end
 	
 	-- draw main background
@@ -860,8 +882,8 @@ function generateRoom(floor)
 
 	for y=3, screenHeight do
 		for x=1, screenWidth do
-			envSpriteY = math.floor((map[y][x]-(spriteRowOffsets['environment']*16))/16)
-			envSpriteX = math.floor((map[y][x]-(spriteRowOffsets['environment']*16)))-(envSpriteY*16)
+			envSpriteY = math.floor((map.array[y][x]-(spriteRowOffsets['environment']*16))/16)
+			envSpriteX = math.floor((map.array[y][x]-(spriteRowOffsets['environment']*16)))-(envSpriteY*16)
 
 			love.graphics.draws( environment, (x*gridSize)-24, (y*gridSize)-24, (envSpriteX*gridSize), (envSpriteY*gridSize),gridSize, gridSize )
 		end
@@ -897,8 +919,11 @@ function checkXYEmpty(x,y,id)
 			end
 		end
 	end
-	if tileProperties[map[y][x]] == 'solid' then
+	if tileProperties[map.array[y][x]] == 'solid' then
 		occupied = occupied + 1
+	end
+	if player['x'] == x and player['y'] == y then
+		occupied = 1
 	end
 	if occupied > 0 then
 		return false
@@ -918,7 +943,7 @@ function checkSpaceEmpty(space)
 		occupied = 1
 	end
 	spaceXY = getXYFromSpace(space)
-	if tileProperties[map[spaceXY['y']][spaceXY['x']]] == 'solid' then
+	if tileProperties[map.array[spaceXY['y']][spaceXY['x']]] == 'solid' then
 		occupied = 1
 	end
 	if occupied == 1 then
